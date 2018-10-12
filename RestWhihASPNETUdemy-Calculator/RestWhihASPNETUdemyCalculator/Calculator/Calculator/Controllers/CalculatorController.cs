20e75cc1ea195e12995944ad3da228d6e7589272
Marcelo Tavares Vital Dos Santos <marcelo.tavares.vital@gmail.com>
2018-10-12 00:00:25 -0300

    Implementando Calculadora REST

---
+++ RestWhihASPNETUdemy-Calculator/RestWhihASPNETUdemyCalculator/Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (Isnumeric(firstNumber) && Isnumeric(secondNumber))
+            {
+                var sum = CovertToDecimal(firstNumber) + CovertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input"); ;
+        }
+
+        private decimal CovertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool Isnumeric(string number)
+        {
+            double numberNumeric;
+            bool isnumber = double.TryParse(number, System.Globalization.NumberStyles.Any,
+                                            System.Globalization.NumberFormatInfo.InvariantInfo,
+                                            out numberNumeric);
+            return isnumber;
+
+        }
+    }
+}
